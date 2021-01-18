452598cb43e5353ff0eb3a50b25efbe88d9db8a8
Mike Carter <elite.mikecarter@gmail.com>
2021-01-18 06:49:07 -0500

    edited markedown page

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ const questions = [
 
     {
         type: 'input',
-        name: 'Github',
+        name: 'github',
         message: 'What is your GitHub username?',
     },
     {
         type: 'input',
-        name: 'Email',
+        name: 'email',
         message: 'What is your email address?',
     },
     {
         type: 'input',
-        name: 'Title',
+        name: 'title',
         message: 'What is your progect title name?',
 
     },
     {
         type: 'input',
-        name: 'Description',
+        name: 'description',
         message: 'Please write a short description of your project?',
 
     },
@@ -38,31 +38,54 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'Installation',
+        name: 'installation',
         message: 'What command should be run to install dependencies?',
         default: 'npm i inquirer',
 
     },
     {
         type: 'input',
-        name: 'Test',
+        name: 'test',
         message: 'What command should be run to run the tests?',
         default: 'npm test',
 
     },
     {
         type: 'input',
-        name: 'Usage',
+        name: 'usage',
         message: 'What does the user need to know about using the repo?',
 
     },
     {
         type: 'input',
-        name: 'Contributing',
+        name: 'contributing',
         message: 'What does the user need to know about contributing to the repo?',
 
     },
 ];
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">:${answers.title}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
