755882ce07431cfa3c407219ab98222c79cd3129
Mike Carter <elite.mikecarter@gmail.com>
2021-01-18 22:43:23 -0500

    corrected err

--- README 2.md
+++ README 2.md
@@ -8,6 +8,7 @@ You can quickly and easily create a README file by using a command-line applicat
 
 Your task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer). Review the [Good README Guide](../../01-HTML-Git-CSS/04-Important/Good-README-Guide/README.md) as a reminder of everything that a high-quality, professional README should contain. 
 
+
 The application will be invoked by using the following command:
 
 ```bash
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,61 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  # Readme Generator
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+​
+## Description
+​
+This project allows users to generate readme files for their application. It uses Node.js
+​
+## Table of Contents 
+​
+* [Installation](#installation)
+​
+* [Usage](#usage)
+​
+* [License](#license)
+​
+* [Contributing](#contributing)
+​
+* [Tests](#tests)
+​
+* [Questions](#questions)
+​
+## Installation
+​
+To install necessary dependencies, run the following command:
+​
+```
+// npm i
+```
+​
+## Usage
+​${data.usage}
+use git clone to pull down the repo 
+​
+## License
+​​${data.license}
+This project is licensed under the MIT license.
+  
+## Contributing
+​​${data.contribution}
+users cannot contribute to this repo
+​
+## Tests
+​​${data.test}
+To run tests, run the following command:
+​
+```
+// npm test
+```
+​
+## Questions
+​
+If you have any questions about the repo, open an issue or contact me directly at elit.mikecarter@gmail.com. You can find more of my work at [Mike Carter](https://github.com/codehawkeye).
 
+  
 `;
 }
 
